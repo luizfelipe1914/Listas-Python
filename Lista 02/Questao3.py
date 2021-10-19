d1bb5510746d8d998599fb63c8bfd3e33d69d858
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-18 23:06:05 -0300

    Resolução da Questão 03.

--- Lista 02/Questao3.py
+++ Lista 02/Questao3.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 #-*- encoding: UTF-8 -*-
 
+from math import sqrt
+
 def main():
+    try:
+        number = int(input("Informe um número: "))
+    except:
+        print("Apenas valores numéricos devem ser informados!")
+    if(number < 0):
+        print(f"{number}^2 = {number**2}.")
+    else:
+        print(f"sqrt({number}) = {sqrt(number)}")
 
 
 if(__name__ == "__main__"):
