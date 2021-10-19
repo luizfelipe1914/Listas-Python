51db2e7e7395d2a1af3f5df28f8795474589f521
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-18 23:12:16 -0300

    Resolução da Questão 04.

--- Lista 02/Questao4.py
+++ Lista 02/Questao4.py
@@ -2,7 +2,14 @@
 #-*- encoding: UTF-8 -*-
 
 def main():
-
-
+    try:
+        number = int(input("Informe um número: "))
+    except:
+        print("Apenas valores numéricos devem ser informados!")
+    if(number < 0):
+        print(f"{number} é negativo.")
+    else:
+        print(f"{number}^2 = {number**2}")
+        print(f"{number}^(1/2) = {number**(1/2)}")
 if(__name__ == "__main__"):
     main()
