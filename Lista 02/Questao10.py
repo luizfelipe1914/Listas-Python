3c0612ab0d4cc15ebdda5d23b09f8b70db7038af
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-21 00:27:17 -0300

    Resolução da questão 10.

--- Lista 02/Questao10.py
+++ Lista 02/Questao10.py
@@ -2,7 +2,18 @@
 #-*- encoding: UTF-8 -*-
 
 def main():
+    try:
+        sexo = input("Qual o sexo(biológico), [M]asculino ou [F]eminino? ")
+        altura = float(input("Qual a altura? "))
+    except:
+        print("Parâmetros inválidos!")
 
+    if(sexo.upper() == "M"):
+        print(f"Seu peso ideal é: {72.7*altura -58} kg")
+    elif(sexo.upper() == "F"):
+        print(f"Seu peso ideal é: {62.1*altura -44.7} kg")
+    else:
+        print("Entrada inválida!")
 
 if(__name__ == "__main__"):
     main()
