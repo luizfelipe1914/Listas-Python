687e72f7cb2662ccf672d81b39d475cab089fbc0
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-20 21:42:57 -0300

    Resolução da questão 09.

--- Lista 02/Questao9.py
+++ Lista 02/Questao9.py
@@ -2,7 +2,14 @@
 #-*- encoding: UTF-8 -*-
 
 def main():
-
-
+    try:
+        parcela = float(input("Valor da prestação: "))
+        salario = float(input("Salário: "))
+    except:
+        print("Apenas valores numéricos devem ser informados!")
+    if(parcela <= (salario*0.2)):
+        print("Empréstimo concedido!")
+    else:
+        print("Empréstimo não concedido!G")
 if(__name__ == "__main__"):
     main()
