e7a8e1a15d942dcff51b9703b7318d1040068fc9
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-20 21:37:44 -0300

    Resolução da questão 08.

--- Lista 02/Questao8.py
+++ Lista 02/Questao8.py
@@ -2,7 +2,14 @@
 #-*- encoding: UTF-8 -*-
 
 def main():
-
-
+    try:
+        nota1 = float(input("1ª nota: "))
+        nota2 = float(input("2ª nota: "))
+    except:
+        print("Apenas valores numéricos devem ser informados!")
+    if(nota1 < 0 or nota1 > 10 or nota2 < 0 or nota2 > 10):
+        print("Notas inválidas!")
+    else:
+        print(f"1ª Nota: {nota1}\n2ª Nota: {nota2}\nMédia aritmética simples: {(nota1 + nota2)/2}")
 if(__name__ == "__main__"):
     main()
