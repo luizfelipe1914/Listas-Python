805d5cf25facdc77b01ddcf94f9dea9fd516bce4
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-20 21:28:54 -0300

    Resolução da Questão 06.

--- Lista 02/Questao6.py
+++ Lista 02/Questao6.py
@@ -2,7 +2,18 @@
 #-*- encoding: UTF-8 -*-
 
 def main():
-
-
+    try:
+        number1 = int(input("Informe um número: "))
+        number2 = int(input("Informe outro número: "))
+    except:
+        print("Apenas valores numéricos são aceitos!")
+    
+    if(number1 > number2):
+        print(f"{number1} é maior que {number2}. A diferença entre eles é de {number1 - number2}.")
+    elif(number2 > number1):
+        print(f"{number2} é maior que {number1}. A diferença entre eles é de {number2 - number1}.")
+    else:
+        print(f"{number1} = {number2}")
+    
 if(__name__ == "__main__"):
     main()
