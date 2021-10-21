b87500544ff3a879837fc1486bf3d6b7ef94314c
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-21 00:49:04 -0300

    Resolução "mais elaborada" da questão 12.

--- Lista 02/Questao12.py
+++ Lista 02/Questao12.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 #-*- encoding: UTF-8 -*-
 
+from math import log    # Importando o módulo de cálculo de logaritmos
+import random           # Importando o módulo random
+from datetime import datetime   # Importando o módulo datetime para obter-se o horário atual da máquina
+
 def main():
+    random.seed(datetime.now()) # A "semente" para "geração" o numero inteiro será o horário atual da máquina
+    num = random.randint(1,10)  # Será "gerado" um inteiro entre 1 e 10. Ele será a base do logarítmo.
+    try:
+        number = float(input("Informe um número: "))
+    except:
+        print("Apenas valores numéricos devem ser informados!")
+    if(number < 0):
+        print("Número inválido.")
+    else:
+        print(f"O logarítmo de base {num} de {number} é: {log(num, number)}")
 
 
 if(__name__ == "__main__"):
