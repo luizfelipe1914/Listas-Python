1fca1ac8861aebf8c9076095cd29e8e687447e33
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-20 21:23:13 -0300

    Resolução da questão 05.

--- Lista 02/Questao5.py
+++ Lista 02/Questao5.py
@@ -2,6 +2,14 @@
 #-*- encoding: UTF-8 -*-
 
 def main():
+    try:
+        number = int(input("Informe um número: "))
+    except:
+        print("Apenas valores numéricos são aceitos!")
+    if(number % 2 == 0):
+        print(f"{number} é par.")
+    else:
+        print(f"{number} é ímpar.")
 
 
 if(__name__ == "__main__"):
