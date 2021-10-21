2c125fa89cd58e9eaf36e40d0a9581076671dc61
luizfelipe1914 <luizfelipe1914@gmail.com>
2021-10-21 00:27:37 -0300

    Resolução da questão 11.

--- Lista 02/Questao11.py
+++ Lista 02/Questao11.py
@@ -2,7 +2,19 @@
 #-*- encoding: UTF-8 -*-
 
 def main():
-
+    try:
+        number = int(input("Informe um número: "))
+    except:
+        print("Apenas valores numéricos devem ser informados!")
+    if(number < 0):
+        print("Apenas valores positivos devem ser informados!")
+    else:
+        soma = 0
+        aux = number
+        while(aux != 0):
+            soma = soma + (aux % 10)
+            aux = aux // 10
+        print(f"A soma dos algarismo de {number} é {soma}.")
 
 if(__name__ == "__main__"):
     main()
